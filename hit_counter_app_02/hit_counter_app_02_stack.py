d2d9387bf761b429c28376f025769bd9b030034a
ctmithilesh <104360312+ctmithilesh@users.noreply.github.com>
2022-11-24 10:17:04 +0530

    initial commit

--- hit_counter_app_02/hit_counter_app_02_stack.py
+++ hit_counter_app_02/hit_counter_app_02_stack.py
@@ -1,9 +1,13 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_dynamodb as ddb,
+    aws_apigateway as apigw,
 )
 from constructs import Construct
+
+from .hitcounter import HitCounter 
 
 class HitCounterApp02Stack(Stack):
 
@@ -11,9 +15,20 @@ class HitCounterApp02Stack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+                self, 'HelloHandler',
+                runtime=_lambda.Runtime.PYTHON_3_7,
+                code=_lambda.Code.from_asset('lambda'),
+                handler='hello.handler',
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HitCounterApp02Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        hello_with_counter = HitCounter(
+                self, 'HelloHitCounter',
+                downstream=my_lambda,
+        )
+
+        apigw.LambdaRestApi(
+                self, 'Endpoint',
+                handler=hello_with_counter._handler,
+        )
